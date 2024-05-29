038814496387259eb49ead575368d4836a88a2e3
Pino Koegler <ta96sedi@stud.informatik.uni-erlangen.de>
2024-05-29 18:00:11 +0200

    theme

--- lua/pino/packer.lua
+++ lua/pino/packer.lua
@@ -19,8 +19,19 @@ return require('packer').startup(function(use)
 		'nvim-telescope/telescope.nvim', tag = '0.1.6',
 		-- or                            , branch = '0.1.x',
 		requires = { {'nvim-lua/plenary.nvim'} }
-	}
+    }
+    
+    -- === THEME =========================================================
 
+    use({
+        'projekt0n/github-nvim-theme',
+        
+        config = function()
+            vim.cmd('colorscheme github_dark_high_contrast')
+		end
+    })
+
+    --[[
 	use {
 		'rebelot/kanagawa.nvim',
 
@@ -28,6 +39,7 @@ return require('packer').startup(function(use)
 			vim.cmd('colorscheme kanagawa-wave')
 		end
 	}
+    ]]--
 
 	use {
 		'nvim-treesitter/nvim-treesitter',
--- plugin/packer_compiled.lua
+++ plugin/packer_compiled.lua
@@ -109,16 +109,16 @@ _G.packer_plugins = {
     path = "/home/cip/2019/ta96sedi/.local/share/nvim/site/pack/packer/start/friendly-snippets",
     url = "https://github.com/rafamadriz/friendly-snippets"
   },
+  ["github-nvim-theme"] = {
+    config = { "\27LJ\2\nI\0\0\3\0\3\0\0056\0\0\0009\0\1\0'\2\2\0B\0\2\1K\0\1\0*colorscheme github_dark_high_contrast\bcmd\bvim\0" },
+    loaded = true,
+    path = "/home/cip/2019/ta96sedi/.local/share/nvim/site/pack/packer/start/github-nvim-theme",
+    url = "https://github.com/projekt0n/github-nvim-theme"
+  },
   harpoon = {
     loaded = true,
     path = "/home/cip/2019/ta96sedi/.local/share/nvim/site/pack/packer/start/harpoon",
     url = "https://github.com/theprimeagen/harpoon"
-  },
-  ["kanagawa.nvim"] = {
-    config = { "\27LJ\2\n=\0\0\3\0\3\0\0056\0\0\0009\0\1\0'\2\2\0B\0\2\1K\0\1\0\30colorscheme kanagawa-wave\bcmd\bvim\0" },
-    loaded = true,
-    path = "/home/cip/2019/ta96sedi/.local/share/nvim/site/pack/packer/start/kanagawa.nvim",
-    url = "https://github.com/rebelot/kanagawa.nvim"
   },
   ["mason-lspconfig.nvim"] = {
     loaded = true,
@@ -173,10 +173,10 @@ _G.packer_plugins = {
 }
 
 time([[Defining packer_plugins]], false)
--- Config for: kanagawa.nvim
-time([[Config for kanagawa.nvim]], true)
-try_loadstring("\27LJ\2\n=\0\0\3\0\3\0\0056\0\0\0009\0\1\0'\2\2\0B\0\2\1K\0\1\0\30colorscheme kanagawa-wave\bcmd\bvim\0", "config", "kanagawa.nvim")
-time([[Config for kanagawa.nvim]], false)
+-- Config for: github-nvim-theme
+time([[Config for github-nvim-theme]], true)
+try_loadstring("\27LJ\2\nI\0\0\3\0\3\0\0056\0\0\0009\0\1\0'\2\2\0B\0\2\1K\0\1\0*colorscheme github_dark_high_contrast\bcmd\bvim\0", "config", "github-nvim-theme")
+time([[Config for github-nvim-theme]], false)
 
 _G._packer.inside_compile = false
 if _G._packer.needs_bufread == true then
