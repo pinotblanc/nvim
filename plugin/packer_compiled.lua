8bed7fae762d51dbdcfc71305d1729bc96fe8e2a
pinotblanc <pino.koegler@gmail.com>
2024-05-29 18:02:50 +0200

    removed relative line number

--- plugin/packer_compiled.lua
+++ plugin/packer_compiled.lua
@@ -49,8 +49,8 @@ local function save_profiles(threshold)
 end
 
 time([[Luarocks path setup]], true)
-local package_path_str = "/home/cip/2019/ta96sedi/.cache/nvim/packer_hererocks/2.1.1692716794/share/lua/5.1/?.lua;/home/cip/2019/ta96sedi/.cache/nvim/packer_hererocks/2.1.1692716794/share/lua/5.1/?/init.lua;/home/cip/2019/ta96sedi/.cache/nvim/packer_hererocks/2.1.1692716794/lib/luarocks/rocks-5.1/?.lua;/home/cip/2019/ta96sedi/.cache/nvim/packer_hererocks/2.1.1692716794/lib/luarocks/rocks-5.1/?/init.lua"
-local install_cpath_pattern = "/home/cip/2019/ta96sedi/.cache/nvim/packer_hererocks/2.1.1692716794/lib/lua/5.1/?.so"
+local package_path_str = "/home/pino/.cache/nvim/packer_hererocks/2.1.0-beta3/share/lua/5.1/?.lua;/home/pino/.cache/nvim/packer_hererocks/2.1.0-beta3/share/lua/5.1/?/init.lua;/home/pino/.cache/nvim/packer_hererocks/2.1.0-beta3/lib/luarocks/rocks-5.1/?.lua;/home/pino/.cache/nvim/packer_hererocks/2.1.0-beta3/lib/luarocks/rocks-5.1/?/init.lua"
+local install_cpath_pattern = "/home/pino/.cache/nvim/packer_hererocks/2.1.0-beta3/lib/lua/5.1/?.so"
 if not string.find(package.path, package_path_str, 1, true) then
   package.path = package.path .. ';' .. package_path_str
 end
@@ -76,98 +76,98 @@ time([[Defining packer_plugins]], true)
 _G.packer_plugins = {
   LuaSnip = {
     loaded = true,
-    path = "/home/cip/2019/ta96sedi/.local/share/nvim/site/pack/packer/start/LuaSnip",
+    path = "/home/pino/.local/share/nvim/site/pack/packer/start/LuaSnip",
     url = "https://github.com/L3MON4D3/LuaSnip"
   },
   ["cmp-buffer"] = {
     loaded = true,
-    path = "/home/cip/2019/ta96sedi/.local/share/nvim/site/pack/packer/start/cmp-buffer",
+    path = "/home/pino/.local/share/nvim/site/pack/packer/start/cmp-buffer",
     url = "https://github.com/hrsh7th/cmp-buffer"
   },
   ["cmp-nvim-lsp"] = {
     loaded = true,
-    path = "/home/cip/2019/ta96sedi/.local/share/nvim/site/pack/packer/start/cmp-nvim-lsp",
+    path = "/home/pino/.local/share/nvim/site/pack/packer/start/cmp-nvim-lsp",
     url = "https://github.com/hrsh7th/cmp-nvim-lsp"
   },
   ["cmp-nvim-lua"] = {
     loaded = true,
-    path = "/home/cip/2019/ta96sedi/.local/share/nvim/site/pack/packer/start/cmp-nvim-lua",
+    path = "/home/pino/.local/share/nvim/site/pack/packer/start/cmp-nvim-lua",
     url = "https://github.com/hrsh7th/cmp-nvim-lua"
   },
   ["cmp-path"] = {
     loaded = true,
-    path = "/home/cip/2019/ta96sedi/.local/share/nvim/site/pack/packer/start/cmp-path",
+    path = "/home/pino/.local/share/nvim/site/pack/packer/start/cmp-path",
     url = "https://github.com/hrsh7th/cmp-path"
   },
   cmp_luasnip = {
     loaded = true,
-    path = "/home/cip/2019/ta96sedi/.local/share/nvim/site/pack/packer/start/cmp_luasnip",
+    path = "/home/pino/.local/share/nvim/site/pack/packer/start/cmp_luasnip",
     url = "https://github.com/saadparwaiz1/cmp_luasnip"
   },
   ["friendly-snippets"] = {
     loaded = true,
-    path = "/home/cip/2019/ta96sedi/.local/share/nvim/site/pack/packer/start/friendly-snippets",
+    path = "/home/pino/.local/share/nvim/site/pack/packer/start/friendly-snippets",
     url = "https://github.com/rafamadriz/friendly-snippets"
   },
   harpoon = {
     loaded = true,
-    path = "/home/cip/2019/ta96sedi/.local/share/nvim/site/pack/packer/start/harpoon",
+    path = "/home/pino/.local/share/nvim/site/pack/packer/start/harpoon",
     url = "https://github.com/theprimeagen/harpoon"
   },
   ["kanagawa.nvim"] = {
-    config = { "\27LJ\2\n=\0\0\3\0\3\0\0056\0\0\0009\0\1\0'\2\2\0B\0\2\1K\0\1\0\30colorscheme kanagawa-wave\bcmd\bvim\0" },
+    config = { "\27LJ\2\2=\0\0\2\0\3\0\0056\0\0\0009\0\1\0'\1\2\0B\0\2\1K\0\1\0\30colorscheme kanagawa-wave\bcmd\bvim\0" },
     loaded = true,
-    path = "/home/cip/2019/ta96sedi/.local/share/nvim/site/pack/packer/start/kanagawa.nvim",
+    path = "/home/pino/.local/share/nvim/site/pack/packer/start/kanagawa.nvim",
     url = "https://github.com/rebelot/kanagawa.nvim"
   },
   ["mason-lspconfig.nvim"] = {
     loaded = true,
-    path = "/home/cip/2019/ta96sedi/.local/share/nvim/site/pack/packer/start/mason-lspconfig.nvim",
+    path = "/home/pino/.local/share/nvim/site/pack/packer/start/mason-lspconfig.nvim",
     url = "https://github.com/williamboman/mason-lspconfig.nvim"
   },
   ["mason.nvim"] = {
     loaded = true,
-    path = "/home/cip/2019/ta96sedi/.local/share/nvim/site/pack/packer/start/mason.nvim",
+    path = "/home/pino/.local/share/nvim/site/pack/packer/start/mason.nvim",
     url = "https://github.com/williamboman/mason.nvim"
   },
   ["nvim-cmp"] = {
     loaded = true,
-    path = "/home/cip/2019/ta96sedi/.local/share/nvim/site/pack/packer/start/nvim-cmp",
+    path = "/home/pino/.local/share/nvim/site/pack/packer/start/nvim-cmp",
     url = "https://github.com/hrsh7th/nvim-cmp"
   },
   ["nvim-lspconfig"] = {
     loaded = true,
-    path = "/home/cip/2019/ta96sedi/.local/share/nvim/site/pack/packer/start/nvim-lspconfig",
+    path = "/home/pino/.local/share/nvim/site/pack/packer/start/nvim-lspconfig",
     url = "https://github.com/neovim/nvim-lspconfig"
   },
   ["nvim-treesitter"] = {
     loaded = true,
-    path = "/home/cip/2019/ta96sedi/.local/share/nvim/site/pack/packer/start/nvim-treesitter",
+    path = "/home/pino/.local/share/nvim/site/pack/packer/start/nvim-treesitter",
     url = "https://github.com/nvim-treesitter/nvim-treesitter"
   },
   ["packer.nvim"] = {
     loaded = true,
-    path = "/home/cip/2019/ta96sedi/.local/share/nvim/site/pack/packer/start/packer.nvim",
+    path = "/home/pino/.local/share/nvim/site/pack/packer/start/packer.nvim",
     url = "https://github.com/wbthomason/packer.nvim"
   },
   ["plenary.nvim"] = {
     loaded = true,
-    path = "/home/cip/2019/ta96sedi/.local/share/nvim/site/pack/packer/start/plenary.nvim",
+    path = "/home/pino/.local/share/nvim/site/pack/packer/start/plenary.nvim",
     url = "https://github.com/nvim-lua/plenary.nvim"
   },
   ["telescope.nvim"] = {
     loaded = true,
-    path = "/home/cip/2019/ta96sedi/.local/share/nvim/site/pack/packer/start/telescope.nvim",
+    path = "/home/pino/.local/share/nvim/site/pack/packer/start/telescope.nvim",
     url = "https://github.com/nvim-telescope/telescope.nvim"
   },
   undotree = {
     loaded = true,
-    path = "/home/cip/2019/ta96sedi/.local/share/nvim/site/pack/packer/start/undotree",
+    path = "/home/pino/.local/share/nvim/site/pack/packer/start/undotree",
     url = "https://github.com/mbbill/undotree"
   },
   ["vim-fugitive"] = {
     loaded = true,
-    path = "/home/cip/2019/ta96sedi/.local/share/nvim/site/pack/packer/start/vim-fugitive",
+    path = "/home/pino/.local/share/nvim/site/pack/packer/start/vim-fugitive",
     url = "https://github.com/tpope/vim-fugitive"
   }
 }
@@ -175,7 +175,7 @@ _G.packer_plugins = {
 time([[Defining packer_plugins]], false)
 -- Config for: kanagawa.nvim
 time([[Config for kanagawa.nvim]], true)
-try_loadstring("\27LJ\2\n=\0\0\3\0\3\0\0056\0\0\0009\0\1\0'\2\2\0B\0\2\1K\0\1\0\30colorscheme kanagawa-wave\bcmd\bvim\0", "config", "kanagawa.nvim")
+try_loadstring("\27LJ\2\2=\0\0\2\0\3\0\0056\0\0\0009\0\1\0'\1\2\0B\0\2\1K\0\1\0\30colorscheme kanagawa-wave\bcmd\bvim\0", "config", "kanagawa.nvim")
 time([[Config for kanagawa.nvim]], false)
 
 _G._packer.inside_compile = false
